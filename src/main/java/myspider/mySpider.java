e53c71d0d4a44d161b020f8de7941d29a3360f1a
yaoshuoshenme <946863181@qq.com>
2018-09-18 01:40:42 +0800

    2.0

--- src/main/java/myspider/mySpider.java
+++ src/main/java/myspider/mySpider.java
@@ -5,6 +5,6 @@ import us.codecraft.webmagic.pipeline.ConsolePipeline;
 
 public class mySpider {
     public static void main(String[] args) {
-        new Spider(new MyProcessor()).addUrl("http://news.sina.com.cn/c/2018-09-17/doc-ifxeuwwr5287374.shtml").thread(2).addPipeline(new ConsolePipeline()).runAsync();
+        new Spider(new MyProcessor()).addUrl("https://www.sina.com.cn/").thread(2).addPipeline(new ConsolePipeline()).runAsync();
     }
 }
